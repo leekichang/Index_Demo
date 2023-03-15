865415d22745c7de3e9cc86f620c8b78e1401de8
Kichang Lee <72679149+leekichang@users.noreply.github.com>
2023-03-16 02:05:01 +0900

    client test

--- client_test.py
+++ client_test.py
@@ -1,17 +1,63 @@
-import numpy as np
 import socket
+import numpy as np
 import pickle
+import threading
 
-HOST = '1.233.219.178'
-PORT = 9999
+from SegNet import SegNet
 
-def send(data):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    data_bytes = pickle.dumps(data)
-    s.sendall(data_bytes)
-    s.close()
+import warnings
 
-data = np.random.rand(10)
-print(data)
-send(data)
+warnings.filterwarnings(action='ignore')
+
+HOST = '192.168.0.167'
+PORT = 9784
+
+server_address = (HOST, PORT)
+
+class Client:
+    def __init__(self, server_address, feature):
+        self.server_address = server_address
+        self.socket         = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.feature        = feature
+
+    def connect(self):
+        self.socket.connect(self.server_address)
+
+
+    def disconnect(self):
+        self.socket.close()
+
+    def send_request(self):
+        while True:
+            feature_bytes = pickle.dumps(self.feature)
+            self.socket.sendall(feature_bytes)
+            print(f"{self.feature} SENT!")
+            user_input = input("Enter 'Q' to exit, or press any key")
+            if user_input == 'Q':
+                break
+    def recv_response(self):
+        while True:
+            response_msg = self.socket.recv(1024)
+            response = pickle.loads(response_msg)
+            print(response)
+
+    def create_thread(self):
+        self.send_thread = threading.Thread(target=self.send_request)
+        self.recv_thread = threading.Thread(target=self.recv_response)
+
+    def run(self):
+        self.connect()
+        self.create_thread()
+        self.send_thread.start()
+        self.send_thread.join()
+#        self.recv_response()
+        self.recv_thread.start()
+#        self.send_thread.join()
+        self.recv_thread.join()
+        self.disconnect()
+
+
+if __name__ == '__main__':
+    feature = np.random.rand(5)    
+    client = Client(server_address, feature)
+    client.run()
