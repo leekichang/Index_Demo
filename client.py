4af55dc17c6a68f2a3b473baf772392ab60e8c40
Kichang Lee <72679149+leekichang@users.noreply.github.com>
2023-03-20 20:43:41 +0900

    test

--- client.py
+++ client.py
@@ -1,60 +1,115 @@
+#client.py
 import cv2
-
 import torch
-import socket
-import numpy as np
+import threading
+import time
+from socket import *
 import pickle
+import numpy as np
+import utils
 import config as cfg
-
 from SegNet import SegNet
 
 import warnings
 warnings.filterwarnings(action='ignore')
 
-model = SegNet(n_layers=13, n_class=3)
-model.load_state_dict(torch.load('./reconstructor_300.pth', map_location=torch.device(cfg.DEVICE)))
-model.to(cfg.DEVICE)
-model.eval()
-np.random.seed(20000811)
+class Client:
+    def __init__(self, SERVER_ADDRESS):
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.connect(SERVER_ADDRESS)
+        self.connected = True
+        print("Client Connected!")
+        self.model = SegNet(n_layers=13, n_class=3)
+        self.model.load_state_dict(torch.load('./reconstructor_300.pth', map_location=torch.device(cfg.DEVICE)))
+        self.model.to(cfg.DEVICE)
+        self.model.eval()
 
-HOST = '192.168.0.14'
-PORT = 9004
+    def compute(self, request):
+        try:
+            img = np.array(\
+                cv2.imread(f'./{request}_image.png',\
+                cv2.IMREAD_COLOR)/255).transpose(2,0,1)
+            print("IMAGE LOADED!")
+            X = torch.empty((1,3,224,224))
+            X[0] = torch.tensor(img)
+            X = X.to(cfg.DEVICE)
+            with torch.no_grad():
+                features = self.model.encode(X)
+            features = features.to(cfg.DEVICE)
+            indices = []
+            for idx in self.model.indices:
+                indices.append(idx.to('cpu'))
+            feature_dict = {'feature':features, 'indices':indices, 'shapes':self.model.shapes}
+            print("FINISHED LOCAL COMPUTATION")
+            return feature_dict
+        except:
+            print(f"WRONG IMAGE NAME ./{request}_image.png DOESN'T EXIST")
+            return -1
+    
+    def send(self):
+        while True:
+            # try:
+            user_input = int(input('Enter image number:'))
+            if user_input == -1:
+                self.socket.sendall(str(user_input).encode())
+                self.connected = False
+                self.socket.close()
+                print("DISCONNECT!")
+                break
+            else:
+                data = self.compute(request=user_input)
+                if data == -1:
+                    pass
+                else:
+                    data_byte = pickle.dumps(data)
+                    print('step1')
+                    self.socket.sendall(str(len(data_byte)).encode())
+                    print('step2')
+                    time.sleep(1)
+                    print('step3')
+                    self.socket.sendall(data_byte)
+                    print('step4')
+                    print(f"DATA SENT")
+                    print(f"np.shape(data):{np.shape(data)}")
+                    print(f"len(data_byte):{len(data_byte)}")
+                    break
+            # except:
+            #     print('step5')
+            #     pass
 
-def send(features):
-    #features = np.array(features)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    features_bytes = pickle.dumps(features)#.tobytes()
-    s.sendall(features_bytes)
-    s.close()
+    def recv(self):
+        data_total_len     = int(self.socket.recv(1024))
+        left_recv_len      = data_total_len
+        buffer_size        = data_total_len
+        time.sleep(1)
 
-# features = np.random.randn(1,10)
+        recv_data = []
+        while True:
+            chunk = self.socket.recv(buffer_size)
+            recv_data.append(chunk)
+            left_recv_len -= len(chunk)
+            if left_recv_len <= 0:
+                break
+        if not left_recv_len == 0:
+            print("Packet Loss!")
+        else:
+            result = pickle.loads(b"".join(recv_data))
+            print(f'받은 데이터:{result.shape}\n{data_total_len}')
+    
+    def run(self):
+        while True:
+            self.send()
+            if not self.check_connection():break
+            self.recv()
+            if not self.check_connection():break
 
-#img = cv2.imread('./0_image.png', cv2.IMREAD_COLOR)
-img_ = np.array(cv2.imread('./0_image.png', cv2.IMREAD_COLOR)/255).transpose(2,0,1)
+    def check_connection(self):
+        return self.connected
 
-#cv2.imshow('image', img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+if __name__ == '__main__':
+    args = utils.parse_args()
+    SERVER_ADDRESS = (args.ip, args.p)
+    client = Client(SERVER_ADDRESS)
+    client.run()
 
-X = torch.rand((1,3,224,224))
-X[0] = torch.tensor(img_)
-X = X.to(cfg.DEVICE)
 
-with torch.no_grad():
-    features = model.encode(X)
-features = features.to(cfg.DEVICE)
-# print(features.shape)
-
-indices = []
-
-for idx in model.indices:
-    indices.append(idx.to(cfg.DEVICE))
-
-feature_dict = {'feature':features, 'indices':indices, 'shapes':model.shapes}
-
-# send(features.numpy())
-send(feature_dict)
-# print(feature)
-
-# print(feature.shape)
--- server.py
+++ server.py
@@ -1,59 +1,102 @@
-import torch
-import socket
-import numpy as np
+#server.py
+import threading
+import time
+from socket import *
 import pickle
-import cv2
+import numpy as np
+import utils
+import torch
 import config as cfg
-
 from SegNet import SegNet
 
-model = SegNet(n_layers=13, n_class=3)
-model.load_state_dict(torch.load('./reconstructor_300.pth', map_location=torch.device(cfg.DEVICE)))
-model.eval()
-model = model.to(cfg.DEVICE)
+class Server:
+	def __init__(self, SERVER_ADDRESS):
+		self.address  = SERVER_ADDRESS
+		self.socket   = socket(AF_INET, SOCK_STREAM)
+		self.socket.bind(self.address)
+		self.socket.listen(1)
+		print("server is ready")
+		self.client_socket, self.client_addr = self.socket.accept()
+		print(f"server connected by {self.client_addr}")
+		self.request = None
+		self.connected = True
+		self.model = SegNet(n_layers=13, n_class=3)
+		self.model.load_state_dict(torch.load('./reconstructor_300.pth', map_location=torch.device(cfg.DEVICE)))
+		self.model.eval()
+		self.model.to(cfg.DEVICE)
+		
+	def compute(self):
+		feature = self.request['feature']
+		indices = self.request['indices']
+		shapes  = self.request['shapes']
 
-HOST = '0.0.0.0'
-PORT = 9784
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = (HOST, PORT)
-server_socket.bind((HOST, PORT))
-server_socket.listen(1)
+		feature = feature.to(cfg.DEVICE)
+		self.model.set_index(indices, shapes)
+		
+		for i, index in enumerate(self.model.indices):
+			self.model.indices[i] = index.to(cfg.DEVICE)
+			
+		with torch.no_grad():
+			out = self.model.decode(feature)
+			out = out.to('cpu').numpy()
+		return out
 
-def receive_features(client_socket):
-    result_bytes = b''
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        result_bytes += data
+	def send(self):
+		if self.request != None and self.request != -1:
+			data = self.compute()
+			data_byte = pickle.dumps(data)
+			self.client_socket.sendall(str(len(data_byte)).encode())
+			time.sleep(1)
 
-    result = pickle.loads(result_bytes)
-    features = result['feature']
-    indices  = result['indices']
-    shapes   = result['shapes']
-    return features, indices, shapes
-
-msg = 'DONE!'
-msg_bytes = pickle.dumps(msg)
-
-print("Server is ready!")
-while True:
-	client_socket, client_address = server_socket.accept()
-	print('Connected by', client_address)
-	features, indices, shapes = receive_features(client_socket)
-	print("RECEIVCED THE FEATURES START OPERATION")
+			self.client_socket.sendall(data_byte)
+			print(f"DATA SENT")
+			print(f"np.shape(data):{np.shape(data)}")
+			print(f"len(data_byte):{len(data_byte)}")
+			self.request = None
         
-	features = features.to(cfg.DEVICE)
-	model.set_index(indices, shapes)
-	for i, index in enumerate(model.indices):
-		model.indices[i] = index.to(cfg.DEVICE)
-	with torch.no_grad():
-		out = model.decode(features)
-		out = out.to('cpu').numpy()
-	# img = out[0].transpose(1,2,0)*255
-	# cv2.imwrite('./result.png', img)
-	# img = cv2.imread('./result.png')
-	print(f'server side operation for {client_address} is done')
-	client_socket.sendall(msg_bytes)
-	print("Sent check msg")
-	client_socket.close()
+	def recv(self):
+		data_total_len = int(self.client_socket.recv(1024))
+		left_recv_len  = data_total_len
+		buffer_size    = data_total_len
+		
+		if data_total_len == -1:
+			print("DISCONNECTED!")
+			self.connected = False
+			return
+		
+		time.sleep(1)
+
+		recv_data = []
+		while True:
+			chunk = self.client_socket.recv(buffer_size)
+			recv_data.append(chunk)
+			left_recv_len -= len(chunk)
+			if left_recv_len <= 0:
+				break
+		if not left_recv_len == 0:
+			print("Packet Loss!")
+		else:
+			self.request = pickle.loads(b"".join(recv_data))
+			print(f'받은 데이터:{type(self.request)}\nlen(data):{data_total_len}')
+        
+	def connect(self):
+		print("WAITING FOR NEW CONNECTION")
+		self.socket.listen(1)
+		self.client_socket, self.client_addr = self.socket.accept()
+		print(f"server connected by {self.client_addr}")
+		self.connected = True
+    
+	def run(self):
+		while True:
+			self.recv()
+			self.send()
+			if not self.check_connection():self.connect()
+
+	def check_connection(self):
+		return self.connected
+
+if __name__ == '__main__':
+    args = utils.parse_args()
+    SERVER_ADDRESS = ('0.0.0.0', args.p)
+    server = Server(SERVER_ADDRESS)
+    server.run()
